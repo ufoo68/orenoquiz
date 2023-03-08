9326f9641bf2a1fa6a778031ce41c08fe7d49e8f
ufoo68 <mitarou.yuutarou@gmail.com>
2023-03-08 11:31:23 +0900

    文字選択対策

--- src/components/quizParticipant/item/SortItems.tsx
+++ src/components/quizParticipant/item/SortItems.tsx
@@ -129,7 +129,7 @@ const SortableItem: FC<ItemProps> = ({ id, label }) => {
           d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
         />
       </svg>
-      <div>{label}</div>
+      <div className="select-none">{label}</div>
     </div>
   )
 }
