f382ecce4849ff010e8f40132500c4a02338b469
ufoo68 <mitarou.yuutarou@gmail.com]>
2023-03-08 23:32:59 +0900

    add manifest

---
+++ src/pages/_document.tsx
@@ -0,0 +1,31 @@
+import type { DocumentContext, DocumentInitialProps } from 'next/document'
+import Document, { Html, Head, Main, NextScript } from 'next/document'
+
+class MyDocument extends Document {
+  static async getInitialProps(
+    ctx: DocumentContext
+  ): Promise<DocumentInitialProps> {
+    return await Document.getInitialProps(ctx)
+  }
+
+  render() {
+    return (
+      <Html lang="ja-JP" dir="ltr">
+        <Head>
+          <meta name="application-name" content="orenoquiz" />
+          <meta name="theme-color" content="#000" />
+          <meta name="description" content="quiz application for party" />
+          <link rel="icon" sizes="192x192" href="/icon.jpg" />
+          <link rel="icon" href="/favicon.ico" />
+          <link rel="manifest" href="/manifest.json" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
+
+export default MyDocument
