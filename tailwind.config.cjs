c389d33bc7b1650b0d32beea4de661ce9a9fe0ae
ufoo68 <mitarou.yuutarou@gmail.com>
2024-10-27 18:29:08 +0900

    disable darkmode

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,9 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   mode: 'jit',
-  content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  content: ['./src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {},
   },
-  plugins: [require("daisyui")],
-};
+  plugins: [require('daisyui')],
+  daisyui: {
+    themes: false,
+    styled: true,
+    base: true,
+    utils: true,
+    logs: true,
+  },
+}
